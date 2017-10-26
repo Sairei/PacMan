324a9f9cb67e98fad04f668dd0dc9e0627ae8def
Sairei <christophercauet@hotmail.fr>
2017-10-26 18:07:53 +0200

    joueur.cpp

--- joueur.cpp
+++ joueur.cpp
@@ -8,3 +8,20 @@ joueur::joueur(int x, int y) : entite(x, y)
     this->dir[0] = 0;
     this->dir[1] = -1;
 }
+
+
+int joueur::GetBuffer()
+{
+    return buffer_event;
+}
+
+void joueur::SetBuffer(int e)
+{
+    buffer_event = e;
+}
+
+
+bool joueur::Move(int Event)
+{
+
+}
--- joueur.h
+++ joueur.h
@@ -5,9 +5,15 @@
 
 class joueur : public entite
 {
+    private:
+        int buffer_event;
+
     public:
         joueur(int x, int y);
         bool Move(int Event);
+
+        int GetBuffer();
+        void SetBuffer(int e);
 };
 
 #endif // JOUEUR_H
